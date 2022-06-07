3379fccf657dd96f46cc543d91a2fdad859cdaa6
Hugo Ferrand <hugo.ferrand98@audencia.com>
2022-06-07 09:24:21 +0200

    search bar done

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,8 @@
 class RankingsController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def home
+    @users = User.all
   end
 
   def classement
@@ -7,5 +10,60 @@ class RankingsController < ApplicationController
   end
 
   def monclassement
+    @users = [
+
+      "hugoferrand \r\n\nClassé 10ème
+      \r\n\nPari 1 sur Zara : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 2 sur VIX : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 3 sur Eur : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 4 sur Amazon : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 5 sur Total : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 6 sur BTC : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 7 sur Luna : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 8 sur Gold : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 9 sur Vinci : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 10 sur CAC40 : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'",
+
+      "henri_chtl \r\n\nClassé 9ème
+      \r\n\nPari 1 sur Zara : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 2 sur VIX : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 3 sur Eur : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 4 sur Amazon : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 5 sur Total : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 6 sur BTC : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 7 sur Luna : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 8 sur Gold : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 9 sur Vinci : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 10 sur CAC40 : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'",
+
+      "plouisprevel \r\n\nClassé 9ème
+      \r\n\nPari 1 sur Zara : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 2 sur VIX : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 3 sur Eur : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 4 sur Amazon : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 5 sur Total : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 6 sur BTC : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 7 sur Luna : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 8 sur Gold : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 9 sur Vinci : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 10 sur CAC40 : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'",
+
+      "paul.lewiswbr \r\n\nClassé 9ème
+      \r\n\nPari 1 sur Zara : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 2 sur VIX : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 3 sur Eur : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 4 sur Amazon : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 5 sur Total : \r\nPerdu \r\nTa réponse était 'Stable' \r\nLa bonne réponse était 'Augmenter'
+      \r\n\nPari 6 sur BTC : \r\nGagné \r\nTa réponse était '100' \r\nLa bonne réponse était '100'
+      \r\n\nPari 7 sur Luna : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 8 sur Gold : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'
+      \r\n\nPari 9 sur Vinci : \r\nGagné \r\nTa réponse était '+10%' \r\nLa bonne réponse était '10%'
+      \r\n\nPari 10 sur CAC40 : \r\Perdu \r\nTa réponse était '[<100]' \r\nLa bonne réponse était '[>100]'",
+
+    ]
+
+    if params[:word]
+      @users = @users.select { |user| user.start_with?(params[:word]) }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'rankings#home'
   get 'classement', to: 'rankings#classement'
-  get 'monclassement', to: 'rankings#monclassement'
+  post 'monclassement', to: 'rankings#monclassement'
 end
